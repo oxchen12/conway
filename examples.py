7b1de1487f509a84f76f3412a3c9db49d6024678
Oliver Chen <emailoxc@gmail.com>
2021-10-27 12:38:30 -0400

    Added stones

--- examples.py
+++ examples.py
@@ -1,4 +1,4 @@
-# Sourced from https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
+# Examples sourced from https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
 
 
 def replace_at(grid: list[list[int]], subgrid: list[list[int]], start_x: int, start_y: int) -> None:
@@ -9,7 +9,6 @@ def replace_at(grid: list[list[int]], subgrid: list[list[int]], start_x: int, st
 
 
 def get_example(name: str, grid_width: int, grid_height: int) -> list[list[int]]:
-    midgrid = lambda sub, w=grid_width, h=grid_height: (sub, w // 2 - len(sub[0]) // 2, h // 2 - len(sub) // 2)
     out: list[list[int]] = [[0 for i in range(grid_width)] for j in range(grid_height)]
     match name.lower().strip():
         # oscillators
@@ -53,8 +52,14 @@ def get_example(name: str, grid_width: int, grid_height: int) -> list[list[int]]
             subgrid = [[0, 1, 0, 0, 0, 0, 0],
                        [0, 0, 0, 1, 0, 0, 0],
                        [1, 1, 0, 0, 1, 1, 1]]
+        case "stones":
+            subgrid = [[1, 1, 1, 0, 1],
+                       [1, 0, 0, 0, 0],
+                       [0, 0, 0, 1, 1],
+                       [0, 1, 1, 0, 1],
+                       [1, 0, 1, 0, 1]]
         # no example exists
         case _:
             raise ValueError(f"Couldn't find example {name}")
-    replace_at(out, *midgrid(subgrid))
+    replace_at(out, subgrid, grid_width // 2 - len(subgrid) // 2, grid_height // 2 - len(subgrid) // 2)
     return out
