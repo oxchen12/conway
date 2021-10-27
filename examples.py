3bf009814a144261c0096cb644e73500f899d7b2
Oliver Chen <emailoxc@gmail.com>
2021-10-27 12:30:44 -0400

    Refactored empty()

--- examples.py
+++ examples.py
@@ -1,8 +1,5 @@
 # Sourced from https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
 
-def empty() -> list[list[int]]:
-    return [[0 for i in range(100)] for j in range(100)]
-
 
 def replace_at(grid: list[list[int]], subgrid: list[list[int]], start_x: int, start_y: int) -> None:
     end_x = start_x + len(subgrid[0])
@@ -13,7 +10,7 @@ def replace_at(grid: list[list[int]], subgrid: list[list[int]], start_x: int, st
 
 def get_example(name: str, grid_width: int, grid_height: int) -> list[list[int]]:
     midgrid = lambda sub, w=grid_width, h=grid_height: (sub, w // 2 - len(sub[0]) // 2, h // 2 - len(sub) // 2)
-    out: list[list[int]] = empty()
+    out: list[list[int]] = [[0 for i in range(grid_width)] for j in range(grid_height)]
     match name.lower().strip():
         # oscillators
         case "blinker":
